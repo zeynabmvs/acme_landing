58f166f25589cf46deb925497f8c9aacb8724085
Zeynab Movasaqpoor <zeynab.mvs@gmail.com>
2023-06-27 16:53:34 +0330

    fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,27 @@
+
+const swiper = new Swiper('.swiper', {
+    loop: true,
+
+    // Navigation arrows
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+
+});
+
+//  add customer's picture to testimonial's background
 var testimonial = document.getElementById('testimonial1');
 var bg_url = ''
 
-console.log(testimonial)
-console.log(bg_url)
-
 testimonial.addEventListener('mouseenter', function() {
-    console.log("hi")
     bg_url = document.querySelector('.swiper-slide-active img').getAttribute('src');
-    testimonial.style.setProperty('--before-background-image', 'url('+ bg_url + ')');
+    testimonial.style.setProperty('--before-background-image', 'url(/'+ bg_url + ')');
 })
 
-
 testimonial.addEventListener('mouseleave', function() {
-    console.log("bye")
     bg_url = document.querySelector('.swiper-slide-active img').getAttribute('src');
     testimonial.style.setProperty('--before-background-image', 'url(" ")');
 })
+
+
